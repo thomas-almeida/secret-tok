63d3577515b5b71d75361e6418da1c6bc1f0eb82
thomas-almeida <contato.thomasalmeidard@gmail.com>
2026-03-08 22:14:07 -0300

    [FRONT] Suspense

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,21 @@
 "use client"
 
 import { useAffiliateCode } from "./hooks/useAffiliateCode"
-import VideoFeed from "./components/video-feed"
+import dynamic from 'next/dynamic'
+import { Suspense } from 'react'
+
+const VideoFeed = dynamic(() => import('./components/video-feed'), {
+  ssr: false,
+  loading: () => <div className="h-full w-full bg-black"></div>
+})
 
 export default function Home() {
   useAffiliateCode()
   return (
     <main className="h-dvh w-full bg-background">
-      <VideoFeed />
+      <Suspense fallback={<div className="h-full w-full bg-black"></div>}> 
+        <VideoFeed />
+      </Suspense>
     </main>
   )
 }
